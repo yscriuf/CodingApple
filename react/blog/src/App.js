4bab10aad30b29cd42a9450fc3ddf2cde980712c
Jirong <munja__@naver.com>
2022-07-03 23:00:37 +0900

    style 속성 넣고싶을 때는 style = {} 안에 객체 형태로 스타일을 주어야한다. font-size 같이 -가 붙은것들은 font라는 변수 - size라는 변수로 인식하기 때문에 fontSize로 바꿔서 작성해주어야한다.

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   let blogName = "지렁이의 성장 블로그";
-  
+
   return (
     <div className="App">
       <div className="blog-title">
         {blogName}
+      </div>
+      <div style={ {color : 'black', fontSize : '30px'} }>
+        게시글
       </div>
     </div>
   );
