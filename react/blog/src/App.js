b11487b8e99ee79f6f90fd4e06e0777e311e7b4b
Jirong <munja__@naver.com>
2022-07-05 01:15:51 +0900

    따봉버튼 하나 만들어서 버튼 누를 시 숫자 올라가도록. useState를 이용함

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -5,6 +5,11 @@ import { useState } from 'react';
 function App() {
   let blogName = "지렁이의 성장 블로그";
   let [title, b] = useState(['남자 코트 추천', '여자 코트 추천', '할머니 코트 추천']);
+  let [like, upLike] = useState(0);
+
+  function upCntLike(){
+    upLike(like+1);
+  }
 
   return (
     <div className="App">
@@ -15,7 +20,7 @@ function App() {
         게시글
       </div>
       <div className="list">
-        <h4>{title[0]}</h4>
+        <h4>{title[0]} <span onClick={ upCntLike }>👍</span> {like} </h4>
         <p>2월 16일 발행</p>
       </div>
       <div className="list">
