f226af6902e043d8718ba26a3ac6270fb6e33a86
Jirong <munja__@naver.com>
2022-07-05 01:30:39 +0900

    useState 배열으로 주고받기

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -4,11 +4,19 @@ import { useState } from 'react';
 
 function App() {
   let blogName = "지렁이의 성장 블로그";
-  let [title, b] = useState(['남자 코트 추천', '여자 코트 추천', '할머니 코트 추천']);
-  let [like, upLike] = useState(0);
+  let [title, changeTitle] = useState(['남자 코트 추천', '여자 코트 추천', '할머니 코트 추천']);
+  let [like, upLike] = useState([0,0,0]);
 
-  function upCntLike(){
-    upLike(like+1);
+  function upCntLike(i){
+    var newLike = [...like];
+    newLike[i] = newLike[i] + 1;
+    upLike(newLike);
+  }
+
+  function change(i, txt){
+    var newTitle = [...title];
+    newTitle[i] = txt;
+    changeTitle(newTitle);
   }
 
   return (
@@ -20,15 +28,16 @@ function App() {
         게시글
       </div>
       <div className="list">
-        <h4>{title[0]} <span onClick={ upCntLike }>👍</span> {like} </h4>
+        <h4>{title[0]} <span onClick={ () => { upCntLike(0) } }>👍</span> {like[0]} </h4>
+        <p>2월 16일 발행</p>
+        <button onClick={ () => { change(0, 'hihi'); } }>다른 글 보기</button>
+      </div>
+      <div className="list">
+        <h4>{title[1]} <span onClick={ () => { upCntLike(1) } }>👍</span> {like[1]} </h4>
         <p>2월 16일 발행</p>
       </div>
       <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 16일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
+        <h4>{title[2]} <span onClick={ () => { upCntLike(2) } }>👍</span> {like[2]} </h4>
         <p>2월 16일 발행</p>
       </div>
     </div>
