d2012d34ec7a6e0bed5c90095ccda6def568b567
Jirong <munja__@naver.com>
2022-07-06 00:37:51 +0900

    아이템을 눌렀을 때 그 아이템의 제목이 modal 창에 뜨도록 코드 수정

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -6,7 +6,7 @@ function App() {
   let blogName = "지렁이의 성장 블로그";
   let [title, changeTitle] = useState(['할머니 코트 추천', '여자 코트 추천', '남자 코트 추천']);
   let [like, upLike] = useState([0,0,0]);
-  let [modal, setModal] = useState(false);
+  let [modal, setModal] = useState(0);
 
   function upCntLike(i){
     var newLike = [...like];
@@ -35,14 +35,14 @@ function App() {
         title.map((t, i)=>{
           return(
             <div className="list" key={i}>
-              <h4 onClick={() => { modal ? setModal(false) : setModal(true); }}>{title[i]} <span onClick={ () => { upCntLike(i) } }>👍</span> {like[i]} </h4>
+              <h4 onClick={() => { modal == i + 1 ? setModal(0) : setModal(i + 1); }}>{title[i]} <span onClick={ () => { upCntLike(i) } }>👍</span> {like[i]} </h4>
               <p>2월 16일 발행</p>
             </div>
           )
         })
       }
       {
-        modal ? <Modal color="yellow" title={title} order={order} /> : null
+        modal ? <Modal color="yellow" title={title[modal - 1]} /> : null
       }
 
     </div>
@@ -52,10 +52,9 @@ function App() {
 function Modal(props){
   return(
     <div className='modal' style={{background : props.color}}>
-      <h4>{props.title[0]}</h4>
+      <h4>{props.title}</h4>
       <p>날짜</p>
       <p>내용</p>
-      <button onClick={props.order}>정렬</button>
     </div>
   );
 }
