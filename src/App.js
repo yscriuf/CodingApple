5d149c9b1dd3faf697d92e724613380303b6cb2a
Jirong <munja__@naver.com>
2022-07-09 23:40:28 +0900

    react-router-dom 라이브러리를 사용하여 링크로 component 분리

--- src/App.js
+++ src/App.js
@@ -1,46 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import { Button, Navbar, Container, Nav, Col, Row } from 'react-bootstrap';
-import bg from './img/bg.png'
-import { data } from './data';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Button, Navbar, Container, Nav, Col, Row } from "react-bootstrap";
+import bg from "./img/bg.png";
+import { data } from "./data";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   let [shoes] = useState(data);
 
   return (
     <div className="App">
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Link to="/detail">Detail</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg' style={{backgroundImage : 'url(' + bg + ')' }} ></div>
-      
-      <Row>
-      {
-         shoes.map((t, i)=>{
-           return(
-             <ITEMCARD item={shoes[i]}></ITEMCARD>
-           )
-         })        
-      }
-      </Row>
-
+      <Routes>
+        <Route path="/" element={<HOME shoes={shoes} />} />
+        <Route path="/detail" element={ <div><DETAIL></DETAIL></div> }/>
+      </Routes>
     </div>
   );
 }
 
-function ITEMCARD(props){
-  return(
+function DETAIL(props) {
+  <>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  </>;
+}
+
+function HOME(props) {
+  return (
+    <>
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+
+      <Row>
+        {props.shoes.map((t, i) => {
+          return <ITEMCARD item={props.shoes[i]}></ITEMCARD>;
+        })}
+      </Row>
+    </>
+  );
+}
+
+function ITEMCARD(props) {
+  return (
     <Col md={4}>
-      <img src={"https://codingapple1.github.io/shop/shoes" + (props.item.id + 1) + ".jpg"} width='80%'/>
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" +
+          (props.item.id + 1) +
+          ".jpg"
+        }
+        width="80%"
+      />
       <h4>{props.item.title}</h4>
       <p>{props.item.content}</p>
       <p>{props.item.price}</p>
