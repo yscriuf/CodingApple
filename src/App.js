9fa84ade7982ac06f668cfd8da8ff1119a2d75fb
Jirong <munja__@naver.com>
2022-07-09 23:57:54 +0900

    useNavigate를 이용하여 link 수정. navigate(-1)을 하면 뒤로가기도 알아서 구현됨

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import { Button, Navbar, Container, Nav, Col, Row } from "react-bootstrap";
 import { data } from "./data";
 import { HOME } from "./routes/home";
 import { DETAIL } from "./routes/detail";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
+          {/* <Navbar.Brand href="/">ShoeShop</Navbar.Brand> */}
+          <Navbar.Brand onClick={()=>{ navigate('/') }}>ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/detail">Detail</Nav.Link>
+            {/* <Nav.Link href="/detail">Detail</Nav.Link> */}
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
