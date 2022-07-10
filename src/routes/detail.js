799b3bb27a717aa020d4f21f72c837cf86679e0e
Jirong <munja__@naver.com>
2022-07-10 11:34:13 +0900

    life cycle을 이용한 useEffect를 사용하여 mount 시 혹은 dependecy가 변경될 때에만 코드를 실행

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router";
 import styled from "styled-components";
 
 export function DETAIL(props) {
+
+  let [count, setCount] = useState(3);
+  let timer;
+  useEffect(()=>{
+    // 이 안에 적은 코드는, 밑에 html이 렌더링 되고나서 실행되서 빠릿빠릿한 홈페이지처럼 보이게 만들어줌.
+    // mount 시 실행, dependency가 변경될 때 실행
+    console.log('진입');
+    timer = setTimeout(()=>{
+      if(count > 0)
+        setCount(count-1);
+      else
+        clearTimeout(timer);
+    }, 1000);
+  }, [])
 
   let {id} = useParams();
   console.log(id);
@@ -23,7 +39,17 @@ export function DETAIL(props) {
 
   return (
     <div className="container">
-      <ColorBtn bg="white">버튼</ColorBtn>
+      {
+        count ?
+        <>
+          <div className="alert alert-warning">
+            {count}초 이내 아래 버튼 클릭 시 할인
+          </div>
+          <ColorBtn bg="white" onClick={()=>{ alert('할인!') }}>
+            버튼
+          </ColorBtn>
+        </> : null
+      }
       <div className="row">
         <div className="col-md-6">
           <img
