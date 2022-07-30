7764f142c7e981381d6bc74eae01a568e673088b
Jirong <munja__@naver.com>
2022-07-30 22:40:56 +0900

    lazy import 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { createContext, useEffect, useState } from "react";
+import { createContext, lazy, useEffect, useState } from "react";
 import { Button, Navbar, Container, Nav, Col, Row } from "react-bootstrap";
 import { data } from "./data";
 import { HOME } from "./routes/home";
-import { DETAIL } from "./routes/detail";
-import { Cart } from "./routes/Cart";
+// import { DETAIL } from "./routes/detail";
+// import { Cart } from "./routes/Cart";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import axios from "axios";
+import { useQuery } from "react-query";
+
+const DETAIL = lazy(()=> import('./routes/detail.js'));
+const Cart = lazy(()=> import('./routes/Cart.js'));
 
 function App() {
   useEffect(()=>{
@@ -19,6 +23,19 @@ function App() {
   let [context] = useState([0,1,2]);
   let navigate = useNavigate();
 
+  
+  // let result = useQuery('작명', ()=>{
+  //   axios.get('https://codingapple1.github.io/userdata.json').then((a)=>{
+  //     console.log(a.data);
+  //     return a.data
+  //   })
+  // })
+
+  let result = useQuery('작명', ()=>
+    axios.get('https://codingapple1.github.io/userdata.json')
+    .then((a)=>{ return a.data })
+  )
+
   return (
     <div className="App">
       <Navbar bg="primary" variant="dark">
@@ -26,6 +43,9 @@ function App() {
           <Nav className="me-auto">
             <Link to="/cart" className="nav-link">Cart</Link>
             <Link to="/event" className="nav-link">EVENT</Link>
+          </Nav>
+          <Nav className="ms-auto">
+            { result.data }
           </Nav>
       </Navbar>
 
