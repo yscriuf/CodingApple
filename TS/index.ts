c93ac149b33e8bdc554de7b2bbbe1471ce4376a8
Jirong <munja__@naver.com>
2022-08-19 00:59:12 +0900

    typescript narrowing & assertion 과제1

--- TS/index.ts
+++ TS/index.ts
@@ -1,32 +1,18 @@
-// (숙제3) 결혼 가능 확률을 알려주는 함수를 만들어봅시다.
+// (숙제1) 숫자여러개를 array 자료에 저장해놨는데
 
-// 1. 함수의 파라미터로 월소득(만원단위), 집보유여부(true/false), 매력점수 ('상' or '중' or '하') 를 입력할 수 있어야합니다. 
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
 
-// 2. 월소득은 만원 당 1점, 집보유시 500점 & 미보유시 0점, 매력점수는 '상'일 때만 100점으로 계산합니다. 
+// 이걸 클리닝해주는 함수가 필요합니다. 
 
-// 3. 총 점수가 600점 이상일 경우 "결혼가능"을 return 해줘야합니다. 그 외엔 아무것도 return하지 않습니다.
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
 
- 
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
 
-// (예시)
+// 모르는 부분은 구글검색해도 봐드림 
 
-// 결혼가능하냐(700, false, '중') 이렇게 사용할 경우 "결혼가능"을 return 해줍니다.
-
-// 결혼가능하냐(100, false, '상') 이렇게 사용할 경우 아무것도 return되지 않습니다.
-
-
-function 결혼가능하냐(월소득 : number, 집 : boolean, 매력점수 : string) : string | void {
-  let score :number = 0;
-  집 ? score += 500 : null;
-  매력점수 === "상" ? score += 100 : null;
-  score += 월소득;
-  if(score >= 600){
-    return "결혼가능";
-  }
-  else{
-    return;
-  }
+function toPureNumberArray(arr : (number | string)[]) :number[]{
+  return arr.map((d)=> Number(d));
 }
 
-console.log(결혼가능하냐(700, false, '중'));
-console.log(결혼가능하냐(100, false, '상'));
+console.log(toPureNumberArray(['1', '2', '3']));
+console.log(toPureNumberArray([1,2,3]));
