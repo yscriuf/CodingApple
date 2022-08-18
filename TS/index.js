3181c9360122c44c226acd8de23ea1888ed87102
Jirong <munja__@naver.com>
2022-08-19 01:05:22 +0900

    typescript narrowing & assertion 과제2

--- TS/index.js
+++ TS/index.js
@@ -1,11 +1,26 @@
-// (숙제1) 숫자여러개를 array 자료에 저장해놨는데
-// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
-// 이걸 클리닝해주는 함수가 필요합니다. 
-// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
-// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
-// 모르는 부분은 구글검색해도 봐드림 
-function toPureNumberArray(arr) {
-    return arr.map(function (d) { return Number(d); });
+// (숙제2) 다음과 같은 함수를 만들어보십시오.
+// let 철수쌤 = { subject : 'math' }
+// let 영희쌤 = { subject : ['science', 'english'] }
+// let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다. 
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다. 
+// 철수쌤같은 선생님 object 자료를 집어넣으면 
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+// 그리고 타입지정도 엄격하게 해보도록 합시다. 
+// (동작예시)
+// 만들함수( { subject : 'math' } )  //이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } ) //이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )  //이 경우 타입에러 나면 됩니다 
+// Q. 이 자료가 array type 인지 어떻게 검사하냐고요? 구글에 물어보시면 됩니다.
+function 만들함수(obj) {
+    if (Array.isArray(obj.subject)) {
+        return obj.subject[obj.subject.length - 1];
+    }
+    else {
+        return obj.subject;
+    }
 }
-console.log(toPureNumberArray(['1', '2', '3']));
-console.log(toPureNumberArray([1, 2, 3]));
+console.log(만들함수({ subject: 'math' })); //이 경우 'math'를 return
+console.log(만들함수({ subject: ['science', 'art', 'korean'] })); //이 경우 'korean'을 return
+console.log(만들함수({ hello: 'hi' })); //이 경우 타입에러 나면 됩니다 
--- TS/index.ts
+++ TS/index.ts
@@ -1,18 +1,37 @@
-// (숙제1) 숫자여러개를 array 자료에 저장해놨는데
+// (숙제2) 다음과 같은 함수를 만들어보십시오.
 
-// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// let 철수쌤 = { subject : 'math' }
+// let 영희쌤 = { subject : ['science', 'english'] }
+// let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다. 
 
-// 이걸 클리닝해주는 함수가 필요합니다. 
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
 
-// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다. 
 
-// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+// 철수쌤같은 선생님 object 자료를 집어넣으면 
 
-// 모르는 부분은 구글검색해도 봐드림 
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
 
-function toPureNumberArray(arr : (number | string)[]) :number[]{
-  return arr.map((d)=> Number(d));
+// 그리고 타입지정도 엄격하게 해보도록 합시다. 
+
+
+// (동작예시)
+
+// 만들함수( { subject : 'math' } )  //이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } ) //이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )  //이 경우 타입에러 나면 됩니다 
+// Q. 이 자료가 array type 인지 어떻게 검사하냐고요? 구글에 물어보시면 됩니다.
+
+function 만들함수(obj : { subject : string[] | string }) :string {
+  if(Array.isArray(obj.subject)){
+    return obj.subject[obj.subject.length - 1];
+  }
+  else{
+    return obj.subject;
+  }
 }
 
-console.log(toPureNumberArray(['1', '2', '3']));
-console.log(toPureNumberArray([1,2,3]));
+console.log(만들함수( { subject : 'math' } ))  //이 경우 'math'를 return
+console.log(만들함수( { subject : ['science', 'art', 'korean'] } )) //이 경우 'korean'을 return
+console.log(만들함수( { hello : 'hi' } ))  //이 경우 타입에러 나면 됩니다 
