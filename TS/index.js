ce95bfa8a4403a16111386598831993010951ba7
Jirong <munja__@naver.com>
2022-08-19 01:31:20 +0900

    & 연산자를 이용하여 type 객체간의 결합

--- TS/index.js
+++ TS/index.js
@@ -1,10 +1,7 @@
-// (숙제2) 다음 조건을 만족하는 타입을 만들어봅시다. 
-var newObj = {
-    color: "red",
-    size: 15,
-    position: [1, 2, 3]
+// (숙제3) 다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오. 
+var obj = {
+    name: "kim",
+    phone: 123,
+    email: 'abc@naver.com',
+    age: 16
 };
-console.log(newObj);
-newObj.color = "white";
-newObj.size = 13;
-newObj.position = [4, 5, 6];
--- TS/index.ts
+++ TS/index.ts
@@ -1,23 +1,17 @@
-// (숙제2) 다음 조건을 만족하는 타입을 만들어봅시다. 
+// (숙제3) 다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오. 
 
-// 1. 이 타입은 object 자료형이어야합니다.
+// 1. 대충 이렇게 생긴 object 자료를 다룰 일이 많습니다. { name : 'kim', phone : 123, email : 'abc@naver.com' }
 
-// 2. 이 타입은 color 라는 속성을 가질 수도 있으며 항상 문자가 들어와야합니다. 
+// 2. object 안에 있는 이름, 전화번호, 이메일 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
 
-// 3. 이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
+// 3. 각 속성이 어떤 타입일지는 자유롭게 정하십시오. 
 
-// 4. 이 타입은 position 이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야합니다.  
+type Info = { name : string, phone : number, email : string }
+type AgeInfo = Info & { age : number }
 
-type NewType = { color : string, size : number, readonly position : number[] }
-
-let newObj :NewType = {
-  color : "red",
-  size : 15,
-  position : [1, 2, 3]
+let obj :AgeInfo = {
+  name : "kim",
+  phone : 123,
+  email : 'abc@naver.com',
+  age : 16
 }
-
-console.log(newObj)
-
-newObj.color = "white";
-newObj.size = 13;
-newObj.position = [4, 5, 6];
